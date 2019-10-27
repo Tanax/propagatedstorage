14088b6bd3b6a05d0d6ab7e91fbb5ed5719395d3
Marcus Schumann <tanax.ms@gmail.com>
2019-10-27 13:48:34 +0100

    chore: documentation

--- datastore.go
+++ datastore.go
@@ -6,6 +6,10 @@ import (
 
 // Datastore represents how our data store should look
 type Datastore interface {
+	// Get retrieves a propagated storage model that contains the propagated item. Populates
+	// the model passed in by pointer to this method and returns an error if something goes wrong.
 	Get(ctx context.Context, model *Model) error
+	// Save stores a propagated storage model that contains the propagated item in the datastore. Returns
+	// an error if it fails.
 	Save(ctx context.Context, model *Model) error
 }
